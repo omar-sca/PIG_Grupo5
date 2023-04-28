a74e15e99099912e727f7b1ab8f54b7fe86d49ad
omar-sca <128438348+omar-sca@users.noreply.github.com>
2023-04-27 21:11:07 -0300

    Update urls.py

--- productos/urls.py
+++ productos/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.mostrar),
     path('/nuevo', views.nuevo_producto),
     path('/modificar_stock', views.stock),
-    path('/editar/<int:id_prod>', views.editar),
+    path('/editar/<int:id_prod>', views.editar, name='editar'),
 ]
