00879df55ef14b1133a865428771e49abc2ed55e
omar-sca <128438348+omar-sca@users.noreply.github.com>
2023-04-27 21:14:16 -0300

    Delete urls.py

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from productos import views
-
-urlpatterns = [
-    path('', views.mostrar),
-    path('/nuevo', views.nuevo_producto),
-    path('/modificar_stock', views.stock),
-    path('/editar/<int:id_prod>', views.editar, name='editar'),
-]
