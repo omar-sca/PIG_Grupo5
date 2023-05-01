120abfdcee4d87020346858b6af729161f7594c8
R Script <alanc90@gmail.com>
2023-05-01 20:33:29 -0300

    urls

--- productos/urls.py
+++ productos/urls.py
@@ -3,7 +3,7 @@ from productos import views
 
 urlpatterns = [
     path('', views.mostrar),
-    path('/nuevo', views.nuevo_producto),
-    path('/modificar_stock', views.stock),
-    path('/editar/<int:id_prod>', views.editar, name='editar'),
+    path('nuevo/', views.nuevo_producto),
+    path('modificar_stock/', views.stock),
+    path('editar/<int:id_prod>/', views.editar, name='editar'),
 ]
