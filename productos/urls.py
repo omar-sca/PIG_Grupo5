ff1d9ad61edb9c0bd2764e7fe28db8d98048d4d7
omar-sca <128438348+omar-sca@users.noreply.github.com>
2023-05-02 21:52:28 -0300

    Actualizacion urls.py app productos

--- productos/urls.py
+++ productos/urls.py
@@ -2,8 +2,14 @@ from django.urls import path
 from productos import views
 
 urlpatterns = [
-    path('', views.mostrar),
-    path('nuevo/', views.nuevo_producto),
+    path('', views.productos_mostrar),
+    path('nuevo/', views.producto_nuevo),
+    path('editar/<int:id_prod>/', views.producto_editar, name='editar_producto'),
+    
+    path('fabricantes/', views.fabricantes_mostrar),
+    path('fabricantes/nuevo/', views.fabricante_nuevo),
+    path('fabricantes/editar/<int:id_fabr>', views.fabricante_editar, name='editar_fabricante'),
+
     path('modificar_stock/', views.stock),
-    path('editar/<int:id_prod>/', views.editar, name='editar'),
+    path('facturas/', views.ver_facturas),
 ]
