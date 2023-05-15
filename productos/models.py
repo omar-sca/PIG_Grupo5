11fa9165fabb8a4c47a849cab0f3e9f55d33ba9f
R Script <alanc90@gmail.com>
2023-05-15 20:36:58 -0300

    defino modelos

--- productos/models.py
+++ productos/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class Fabricante(models.Model):
+    nombre = models.CharField(max_length=50)
+    direccion = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=20)
+    email = models.EmailField(max_length=50)
+
+    def __str__(self):
+        return self.ManufacturerName
+
+
+class Items(models.Model):
+    nombre = models.CharField(max_length=100)
+    fabricante = models.ForeignKey(Fabricante, on_delete=models.CASCADE)
+    stock = models.IntegerField()
+
+    def __str__(self):
+        return self.ItemName
+
+
+class Facturas(models.Model):
+    numbero = models.CharField(max_length=20)
+    fecha = models.DateField()
+
+    def __str__(self):
+        return self.InvoiceNumber
+
+class FacturaProducto(models.Model):
+    factura = models.ForeignKey(Facturas, on_delete=models.CASCADE)
+    articulo = models.ForeignKey(Items, on_delete=models.CASCADE)
